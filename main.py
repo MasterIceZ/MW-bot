65f03963cb29f726e7573d2699e4ebc8c84997fa
MasterIceZ <64594596+MasterIceZ@users.noreply.github.com>
2022-12-31 00:27:20 +0700

    feat: added slash command! :partly_sunny:

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ load_dotenv()
 token = os.getenv('TOKEN')
 target = os.getenv('TARGET')
 client = commands.Bot()
+guild_list = []
+
+@client.slash_command(name="mwquote", guild_ids=guild_list)
+async def MW(ctx):
+  await ctx.respond("https://media.discordapp.net/attachments/991572180225765447/991572269895798896/unknown.png?width=671&height=671")
 
 @client.event
 async def on_message(msg):
@@ -15,12 +20,13 @@ async def on_message(msg):
   if msg.author.id == target:
     await msg.add_reaction('🇲')
     await msg.add_reaction('🇼')
-    
 
 @client.event
 async def on_ready():
-  print("Bot is now ready to bow MW!")
+  print("Bot is now ready to krab MW!")
   await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="MW"))
+  for guild in client.guilds:
+    guild_list.append(guild.id)
 
 print(token)
 client.run(token)
