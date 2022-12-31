eae981327c7d32355e2d7bb87a37a6a4546abba4
MasterIceZ <64594596+MasterIceZ@users.noreply.github.com>
2022-12-31 07:51:53 +0700

    fix: reaction doesn't work

--- main.py
+++ main.py
@@ -7,17 +7,13 @@ load_dotenv()
 token = os.getenv('TOKEN')
 target = os.getenv('TARGET')
 client = commands.Bot()
-guild_list = []
-
-@client.slash_command(name="mwquote", guild_ids=guild_list)
-async def MW(ctx):
-  await ctx.respond("https://media.discordapp.net/attachments/991572180225765447/991572269895798896/unknown.png?width=671&height=671")
 
 @client.event
 async def on_message(msg):
   if msg.author == client.user:
     return 
-  if msg.author.id == target:
+  if str(msg.author.id) == target:
+    print("Locked Target!")
     await msg.add_reaction('🇲')
     await msg.add_reaction('🇼')
 
@@ -25,8 +21,6 @@ async def on_message(msg):
 async def on_ready():
   print("Bot is now ready to krab MW!")
   await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="MW"))
-  for guild in client.guilds:
-    guild_list.append(guild.id)
 
 print(token)
 client.run(token)
